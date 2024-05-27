e0197c177b605d5d97134d9132aeeb554a1164d1
Vlalden <vladlen@vozhzhaev.ru>
2024-05-27 18:47:39 +0300

    add MultiThread

--- server.py
+++ server.py
@@ -1,13 +1,23 @@
 import socket
-
+import threading
 serverSocket = socket.socket()
 serverSocket.bind(('127.0.0.1', 9123))
 serverSocket.listen(1)
-print("Сервер запущен и ожидает подключение клиента...")
-clientSocket, clientAddr = serverSocket.accept()
-print(f"Клиент подключился {clientAddr}")
+print("Сервер запущен")
+sockets = []
+def chatting(clientSocket):
+    while True:
+        data = clientSocket.recv(1024)
+        print(f"Сообщение от клиента: {data.decode()}")
+        for client in sockets:
+            client.send(data)
 
 while True:
-    data = clientSocket.recv(1024)
-    print(f"Сообщение от клиента: {data.decode()}")
-    clientSocket.send(data.decode().upper().encode())
+    print("ожидает подключение клиента...")
+    clientSocket, clientAddr = serverSocket.accept()
+    sockets.append(clientSocket)
+    print(f"Клиент подключился {clientAddr}")
+    thread = threading.Thread(target=chatting, args=(clientSocket,))
+    thread.start()
+
+
